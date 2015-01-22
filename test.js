1917d18cef23d872f478b8a5f100f7335f269b75
Cédric <cedced19@gmail.com>
2015-01-22 21:00:33 +0100

    Update test

--- test.js
+++ test.js
@@ -1,10 +1,24 @@
 'use strict';
 var checkUpdate = require('./');
 describe('checkUpdate', function () {
-  it('should check for update and display', function () {
+    
+  it('should check for update and display true', function () {
     checkUpdate({
       packageName: 'check-update-tester',
       packageVersion: '0.0.1',
+      isCLI: false
+    }, function (err, lastestVersion, defaultMessage) {
+      if (!err) {
+        console.log(defaultMessage);
+      }
+    });
+  });
+    
+
+  it('should check for update and display false', function () {
+    checkUpdate({
+      packageName: 'check-update-tester',
+      packageVersion: '0.0.2',
       isCLI: false
     }, function (err, lastestVersion, defaultMessage) {
       if (!err) {
