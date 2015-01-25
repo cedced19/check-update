249c41a6e12da0e5d0d299a997d3330d4290746d
Cédric <cedced19@gmail.com>
2015-01-25 11:55:56 +0100

    Optimize code

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 'use strict';
 var latest = require('./lib/latest-version.js'), colors = require('colors'), stringLength = require('./lib/string-length.js');
-var defaultMessage = function (latestVersion, packageVersion, packageName, isCLI) {
+var defaultMessage = function (options) {
   var line1, line2, contentWidth, top, bottom, side, line1rest, line2rest, message;
   var fill = function (str, count) {
     return new Array(count + 1).join(str);
   };
-  if (latestVersion == packageVersion) {
-    line1 = 'You have the latest version of ' + packageName + colors.dim(' (current: ' + packageVersion + ')') + ' ';
+  if (options.latestVersion == options.packageVersion) {
+    line1 = 'You have the latest version of ' + options.packageName + colors.dim(' (current: ' + options.packageVersion + ')') + ' ';
     contentWidth = stringLength(line1);
     top = colors.yellow('\u250C' + fill('\u2500', contentWidth) + '\u2510');
     bottom = colors.yellow('\u2514' + fill('\u2500', contentWidth) + '\u2518');
@@ -15,11 +15,11 @@ var defaultMessage = function (latestVersion, packageVersion, packageName, isCLI
     line1rest = contentWidth - stringLength(line1);
     return '\n\n' + top + '\n' + side + line1 + fill(' ', line1rest) + side + '\n' + bottom + '\n';
   } else {
-    line1 = ' Update available: ' + colors.green.bold(latestVersion) + colors.dim(' (current: ' + packageVersion + ')') + ' ';
-    if (isCLI) {
-      line2 = ' Run ' + colors.blue('npm update -g ' + packageName) + ' to update. ';
+    line1 = ' Update available: ' + colors.green.bold(options.latestVersion) + colors.dim(' (current: ' + options.packageVersion + ')') + ' ';
+    if (options.isCLI) {
+      line2 = ' Run ' + colors.blue('npm update -g ' + options.packageName) + ' to update. ';
     } else {
-      line2 = ' Run ' + colors.blue('npm update ' + packageName) + ' to update. ';
+      line2 = ' Run ' + colors.blue('npm update ' + options.packageName) + ' to update. ';
     }
     contentWidth = Math.max(stringLength(line1), stringLength(line2));
     top = colors.yellow('\u250C' + fill('\u2500', contentWidth) + '\u2510');
@@ -36,7 +36,8 @@ module.exports = function (options, cb) {
       cb(err);
       return;
     }
-    cb(null, latestVersion, defaultMessage(latestVersion, options.packageVersion, options.packageName, options.isCLI));
+    options.latestVersion = latestVersion;
+    cb(null, latestVersion, defaultMessage(options));
     return;
   });
 };
